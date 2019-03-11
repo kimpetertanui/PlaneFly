399f28f4258572e3a75031c6a7142861a312f9f2
kimpetertanui <kimelipeter526@gmail.com>
2019-03-11 19:36:56 +0300

    Added the App ID for publishing

--- app/src/main/java/com/flyplanegame/planefly/SplashScreen.java
+++ app/src/main/java/com/flyplanegame/planefly/SplashScreen.java
@@ -2,17 +2,29 @@ package com.flyplanegame.planefly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
 import ge.xordinate.xengine.EngineActivity;
 
 public class SplashScreen extends EngineActivity {
-
+    private InterstitialAd mInterstitialAd;
+    // private AdView mAdView;
     // Splash screen timer in milliseconds
     private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        showAds();
 
         new Handler().postDelayed(new Runnable() {
 
@@ -32,6 +44,51 @@ public class SplashScreen extends EngineActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
     }
 
-}
+    private void showAds() {
+
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.intertistial_id));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.e("Intertistial Ad TAG", "The interstitial wasn't loaded yet.");
+        }
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.e("Intertistial Ad TAG", "The interstitial Loaded");
+                mInterstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.e("Intertistial Ad TAG", "The interstitial failed "+errorCode);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the interstitial ad is closed.
+            }
+        });
+    }
+
+    }
+
+
